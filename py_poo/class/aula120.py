360c6cd54ae6abd45c8d918c31e51aec0e58aea5
Rafael Brendo <rafalive122@hotmail.com>
2023-10-18 11:19:00 -0300

    Operadores

---
+++ py_poo/class/aula120.py
@@ -0,0 +1,5 @@
+# class -- Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que podem ter seus
+# próprios atributos e metódos.
+# Os objetos gerados pela classe podem usar seus dados internos para realizar várias ações.
+#Por convenção, usamos PascalCase para nomes de classes. 
