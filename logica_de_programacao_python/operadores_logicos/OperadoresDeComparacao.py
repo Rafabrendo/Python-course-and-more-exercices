e98f9839c84674469a6837c94aa120111b9c7e6c
Rafael Brendo <rafalive122@hotmail.com>
2023-10-19 14:58:09 -0300

    Create OperadoresDeComparacao.py

---
+++ logica_de_programacao_python/operadores_logicos/OperadoresDeComparacao.py
@@ -0,0 +1,22 @@
+"""
+Valores truthy e falsy
+Valores avaliados como False são considerados Falsy.
+Valores avaliados como True são considerados Truthy.
+Valores avaliados como Falsy:
+    Listas vazias []
+    Tuplas vazias ()
+    Dicionários vazios {}
+    Sets vazios set()
+    Strings vazias ""
+    Ranges vazios range(0)
+    Números
+
+    Zero de qualquer tipo numérico.
+    Inteiro: 0
+    De ponto flutuante: 0.0
+    Complexo: 0j
+    Constantes
+
+    None
+    False
+"""
