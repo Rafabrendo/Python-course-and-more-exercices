580b782caf3e6859ac142673a82eb3975cfbc2c5
Rafael Brendo <rafalive122@hotmail.com>
2023-11-02 21:26:01 -0300

    Update aula120.py

--- py_poo/class/aula120.py
+++ py_poo/class/aula120.py
@@ -2,4 +2,5 @@
 # As classes geram novos objetos (instâncias) que podem ter seus
 # próprios atributos e metódos.
 # Os objetos gerados pela classe podem usar seus dados internos para realizar várias ações.
-#Por convenção, usamos PascalCase para nomes de classes. 
+#Por convenção, usamos PascalCase para nomes de classes.
+ 
